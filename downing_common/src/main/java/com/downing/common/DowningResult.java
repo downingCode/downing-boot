69fc39689ce1d9f7578c8b4b544b26c4dc7d6224
downing <downingmail@163.com>
2019-06-07 11:54:05 +0800

    统一返回对象

---
+++ downing_common/src/main/java/com/downing/common/DowningResult.java
@@ -0,0 +1,46 @@
+package com.downing.common;
+
+/**
+ * @author downing
+ * @descript 全局请求返回对象
+ */
+public class DowningResult {
+    private Integer code;
+    private String message;
+    private Object data;
+
+    public DowningResult(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public DowningResult(Integer code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
